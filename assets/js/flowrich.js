5e380e0cbc4d5bfde42e7812dd5a34baeb5cef5f
popeashiedu <popeashiedu@gmail.com>
2020-07-09 22:33:58 +0100

    alpha

---
+++ assets/js/flowrich.js
@@ -0,0 +1,9 @@
+// $(function () {
+//     AOS.init({ offset: 300, duration: 1000, easing: "ease-out-quad", once: 0 });
+//     window.addEventListener('load', AOS.refresh);
+// });
+var overlay = document.getElementById("overlay");
+
+window.addEventListener('load', function () {
+    overlay.style.display = 'none';
+})
