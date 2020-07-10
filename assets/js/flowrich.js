78698ca48a51d8d314a8d682c77658b4c327eb4a
popeashiedu <popeashiedu@gmail.com>
2020-07-10 01:03:57 +0100

    White spaces reduced

--- assets/js/flowrich.js
+++ assets/js/flowrich.js
@@ -1,7 +1,7 @@
-// $(function () {
-//     AOS.init({ offset: 300, duration: 1000, easing: "ease-out-quad", once: 0 });
-//     window.addEventListener('load', AOS.refresh);
-// });
+$(function () {
+    AOS.init({ offset: 300, duration: 1000, easing: "ease-out-quad", once: 0 });
+    window.addEventListener('load', AOS.refresh);
+});
 var overlay = document.getElementById("overlay");
 
 window.addEventListener('load', function () {
