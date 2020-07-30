cf256b5a5b5dbc8252cd55813b0102d30a81781f
popeashiedu <popeashiedu@gmail.com>
2020-07-30 09:10:15 +0100

    header bg fixed

--- assets/js/flowrich.js
+++ assets/js/flowrich.js
@@ -1,14 +1,49 @@
-$(function () {
-    AOS.init({ offset: 300, duration: 1000, easing: "ease-out-quad", once: 0 });
-    window.addEventListener('load', AOS.refresh);
-});
+// const { default: Swiper } = require("./swiper");
+
+// PRELOADER CODE 
 var overlay = document.getElementById("overlay");
 
 window.addEventListener('load', function () {
     overlay.style.display = 'none';
 })
+
+
+// NAVBAR COLLAPSE ON CLICK 
 $(document).on("click", ".navbar-collapse", function (e) {
     if ($(e.target).is("a")) {
         $(this).collapse("hide");
     }
 });
+
+
+// AOS.init({ offset: 300, duration: 800 }); AOS ANIMATION INITIALIZER
+$(function () {
+    AOS.init({ offset: 300, duration: 1000, easing: "ease-out-quad", once: 0 });
+    window.addEventListener('load', AOS.refresh);
+});
+
+// INITIALIZE SWIPER 
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+// if (window.innerWidth < 500) {
+//     Swiper.slidesPerView = 1;
+// }
